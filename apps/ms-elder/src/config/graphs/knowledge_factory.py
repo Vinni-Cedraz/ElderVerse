a53ebae6fa47e2de7ef6d3956341bb99d58415ce
rafael-gaia <rafael.gaia@mediacentre.network>
2024-12-10 13:20:53 -0300

    refactor:wip: nats_module, ms-elder export and services - init knowledge graph via factory pattern.

---
+++ apps/ms-elder/src/config/graphs/knowledge_factory.py
@@ -0,0 +1,8 @@
+graph = {
+    "nodes": ["A", "B", "C"],
+    "edges": [
+        {"from": "A", "to": "B", "weight": 1},
+        {"from": "B", "to": "C", "weight": 2},
+        {"from": "C", "to": "A", "weight": 3}
+    ]
+}
