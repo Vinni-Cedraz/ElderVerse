72017f55e687d1bc5cf3bfb0b027a1b6d664f281
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-12-11 16:17:16 -0300

    refactor: separated responsabilities but mixed inheritance with composition by accident

---
+++ apps/ms-elder/src/application/usecases/short_verse.py
@@ -0,0 +1,12 @@
+short_verse = (
+    """Create a reader's digest's style story from the following conversation. 
+            The story should:
+            1. Have an engaging title
+            2. Be structured in clear sections
+            3. Focus on the most interesting life stories and insights shared
+            4. Include direct quotes when relevant
+            5. Have a thoughtful conclusion
+            6. Be between 500-1000 words
+            7. Don't use markdown, because the final format will be a pdf.
+            Format the response with the title on top, followed by the content in paragraphs.""",
+)
